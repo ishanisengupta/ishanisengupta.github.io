c7266954e4416bfcf15e4111028b1b4ec7775003
Ishani Sengupta <is339@cornell.edu>
2023-08-04 18:45:08 -0400

    renamed home.html to index

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const nodemailer = require("nodemailer");
 
-app.get('/', (req, res) =>{
-    res.send('hello!!')
+const PORT = process.env.PORT || 5006;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'akpsiwebsite1@gmail.com',
+            pass: 'mvxyztokypfhueto'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'akpsiwebsite1@gmail.com',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`)
 })
